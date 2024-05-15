2a0249e2cc847d456a1c8eb325a04382c5795273
ilmu23 <ivalimak@student.hive.fi>
2024-05-15 12:12:20 +0300

    Added ex02

---
+++ ex02/src/main.cpp
@@ -0,0 +1,26 @@
+// ███████╗████████╗     ██████╗ ██╗   ██╗████████╗ ██████╗██╗  ██╗ █████╗ ██████╗
+// ██╔════╝╚══██╔══╝     ██╔══██╗██║   ██║╚══██╔══╝██╔════╝██║  ██║██╔══██╗██╔══██╗
+// █████╗     ██║        ██████╔╝██║   ██║   ██║   ██║     ███████║███████║██████╔╝
+// ██╔══╝     ██║        ██╔═══╝ ██║   ██║   ██║   ██║     ██╔══██║██╔══██║██╔══██╗
+// ██║        ██║███████╗██║     ╚██████╔╝   ██║   ╚██████╗██║  ██║██║  ██║██║  ██║
+// ╚═╝        ╚═╝╚══════╝╚═╝      ╚═════╝    ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+//
+// <<main.cpp>>
+
+#include <string>
+#include <iostream>
+
+int32_t	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "&string:    " << &string << std::endl;
+	std::cout << "&stringPTR: " << &stringPTR << std::endl;
+	std::cout << "&stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "string:    " << string << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+}
