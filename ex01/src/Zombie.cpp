ff26ca23f9c7c6c99698520364880df9f9a2b124
ilmu23 <ivalimak@student.hive.fi>
2024-05-15 12:11:24 +0300

    Added ex01

---
+++ ex01/src/Zombie.cpp
@@ -0,0 +1,39 @@
+// ███████╗████████╗     ██████╗ ██╗   ██╗████████╗ ██████╗██╗  ██╗ █████╗ ██████╗
+// ██╔════╝╚══██╔══╝     ██╔══██╗██║   ██║╚══██╔══╝██╔════╝██║  ██║██╔══██╗██╔══██╗
+// █████╗     ██║        ██████╔╝██║   ██║   ██║   ██║     ███████║███████║██████╔╝
+// ██╔══╝     ██║        ██╔═══╝ ██║   ██║   ██║   ██║     ██╔══██║██╔══██║██╔══██╗
+// ██║        ██║███████╗██║     ╚██████╔╝   ██║   ╚██████╗██║  ██║██║  ██║██║  ██║
+// ╚═╝        ╚═╝╚══════╝╚═╝      ╚═════╝    ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+//
+// <<Zombie.cpp>>
+
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie::Zombie(void) {}
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+
+Zombie::Zombie(const Zombie &copy)
+{
+	*this = copy;
+}
+
+Zombie &Zombie::operator=(const Zombie &copy)
+{
+	this->name = copy.name;
+	return *this;
+}
+
+Zombie::~Zombie(void)
+{
+	std::cout << "Zombie " << this->name << " destroyed" << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
