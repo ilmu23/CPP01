99abf577d1f97b384c0cadb8d0fe5ff8e0f6fe85
ilmu23 <ivalimak@student.hive.fi>
2024-05-15 11:22:26 +0300

    Added ex00

---
+++ ex00/inc/Zombie.hpp
@@ -0,0 +1,31 @@
+// ███████╗████████╗     ██████╗ ██╗   ██╗████████╗ ██████╗██╗  ██╗ █████╗ ██████╗
+// ██╔════╝╚══██╔══╝     ██╔══██╗██║   ██║╚══██╔══╝██╔════╝██║  ██║██╔══██╗██╔══██╗
+// █████╗     ██║        ██████╔╝██║   ██║   ██║   ██║     ███████║███████║██████╔╝
+// ██╔══╝     ██║        ██╔═══╝ ██║   ██║   ██║   ██║     ██╔══██║██╔══██║██╔══██╗
+// ██║        ██║███████╗██║     ╚██████╔╝   ██║   ╚██████╗██║  ██║██║  ██║██║  ██║
+// ╚═╝        ╚═╝╚══════╝╚═╝      ╚═════╝    ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+//
+// <<Zombie.hpp>>
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <string>
+
+class Zombie
+{
+	private:
+
+		std::string	name;
+
+	public:
+
+		Zombie(void);
+		Zombie(std::string name);
+		Zombie(const Zombie &copy);
+		Zombie &operator=(const Zombie &copy);
+		~Zombie(void);
+
+		void	announce(void);
+};
+
+#endif
---
+++ ex00/src/main.cpp
@@ -0,0 +1,27 @@
+// ███████╗████████╗     ██████╗ ██╗   ██╗████████╗ ██████╗██╗  ██╗ █████╗ ██████╗
+// ██╔════╝╚══██╔══╝     ██╔══██╗██║   ██║╚══██╔══╝██╔════╝██║  ██║██╔══██╗██╔══██╗
+// █████╗     ██║        ██████╔╝██║   ██║   ██║   ██║     ███████║███████║██████╔╝
+// ██╔══╝     ██║        ██╔═══╝ ██║   ██║   ██║   ██║     ██╔══██║██╔══██║██╔══██╗
+// ██║        ██║███████╗██║     ╚██████╔╝   ██║   ╚██████╗██║  ██║██║  ██║██║  ██║
+// ╚═╝        ╚═╝╚══════╝╚═╝      ╚═════╝    ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+//
+// <<main.cpp>>
+
+#include "Zombie.hpp"
+
+Zombie	*newZombie(std::string name);
+void	randomChump(std::string name);
+
+int32_t	main(void)
+{
+	Zombie	*dumbass;
+	
+	randomChump("Gort");
+	dumbass = newZombie("Joel");
+	randomChump("Cowtrigger");
+	dumbass->announce();
+	randomChump("Draven");
+	dumbass->announce();
+	delete dumbass;
+	return 0;
+}
