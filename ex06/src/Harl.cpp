9fb6afe8f782983078ec6d05afe0791afe6dfd42
ivalimak <ivalimak@student.hive.fi>
2024-06-05 13:19:06 +0300

    Added ex06

---
+++ ex06/src/Harl.cpp
@@ -0,0 +1,104 @@
+// ███████╗████████╗     ██████╗ ██╗   ██╗████████╗ ██████╗██╗  ██╗ █████╗ ██████╗
+// ██╔════╝╚══██╔══╝     ██╔══██╗██║   ██║╚══██╔══╝██╔════╝██║  ██║██╔══██╗██╔══██╗
+// █████╗     ██║        ██████╔╝██║   ██║   ██║   ██║     ███████║███████║██████╔╝
+// ██╔══╝     ██║        ██╔═══╝ ██║   ██║   ██║   ██║     ██╔══██║██╔══██║██╔══██╗
+// ██║        ██║███████╗██║     ╚██████╔╝   ██║   ╚██████╗██║  ██║██║  ██║██║  ██║
+// ╚═╝        ╚═╝╚══════╝╚═╝      ╚═════╝    ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+//
+// <<Harl.cpp>>
+
+#include <iostream>
+#include "Harl.hpp"
+
+Harl::Harl(void) {}
+
+Harl::Harl(const Harl &copy)
+{
+	*this = copy;
+}
+
+Harl	&Harl::operator=(const Harl &copy)
+{
+	return *this;
+	(void)copy;
+}
+
+Harl::~Harl(void) {}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-"
+		<< "special-ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put"
+		<< "enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming"
+		<< "for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	size_t		i;
+	void		(*f[4])(void) = { Harl::debug, Harl::info, Harl::warning, Harl::error };
+	std::string	levels[4] = { "debug", "info", "warning", "error" };
+
+	for (i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+			break ;
+	}
+	if (i == 4)
+		return ;
+	f[i]();
+}
+
+void	Harl::complainFilter(std::string level)
+{
+	size_t		i;
+	std::string	levels[4] = { "debug", "info", "warning", "error" };
+
+	for (i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+			break ;
+	}
+	switch (i)
+	{
+		case 0:
+			std::cout << "[ DEBUG ]" << std::endl;
+			debug();
+			info();
+			warning();
+			error();
+			break ;
+		case 1:
+			std::cout << "[ INFO ]" << std::endl;
+			info();
+			warning();
+			error();
+			break ;
+		case 2:
+			std::cout << "[ WARNING ]" << std::endl;
+			warning();
+			error();
+			break ;
+		case 3:
+			std::cout << "[ ERROR ]" << std::endl;
+			error();
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignifacnt problems ]" << std::endl;
+	}
+}
